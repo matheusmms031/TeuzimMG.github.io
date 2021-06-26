d95ebe39d8c3c80e2f13812646944300d01b1944
Matheus Magalhães <basemagalha@gmail.com>
2021-06-26 18:26:17 -0300

    Update and creating testes

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// window <-- Significa janela, como se fosse o site
+// document <-- Significa Documento, como se fosse o documento do window, no caso o conteudo do site
+// window.document... <-- Busca por documentos dentro do site
+// .getElementsByTagName('{x}') <-- Busca por elementos no document com a tag de nome {x}
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+
+// var texto = String(window.prompt("Digite o primeiro valor"))
+// window.document.write(`<p>${texto.length}</p>`) // .length <-- Largura de texto, no caso caracteres contidos em na variavel, sendo String ou Int
+// window.document.write(`<p>${texto.toLowerCase()}</p>`) // .toLowerCase() <-- Colocar tudo em letra minuscula
+// window.document.write(`<p>${texto.toUpperCase()}</p>`) // .toUpperCase() <-- Colocar tudo em letra maiuscula
+// window.document.write(`<p>${texto.indexOf('teste')}</p>`) // {Element}.indexOf({x}) <-- Procura a string x dentro do Element, caso encontre aparecerá o prieiro número do caracter desejado 
+// window.document.write(`<p>${texto.lastIndexOf('teste')}</p>`) // {Element}.lastIndexOf({x}) <-- Procura a string x dentro do Element, caso encontre aparecerá o ultimo número do caracter desejado
+// window.document.write(`<p>${texto.slice(2,5)}</p>`) // {Element}.slice(start,end) <-- Retorna os valores entre o start e o end
