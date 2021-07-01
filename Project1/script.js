50c475f971b886fee4d080a6e125922cd9d7264d
Matheww <basemagalha@gmail.com>
2021-07-01 19:46:25 -0300

    update

--- Project1/script.js
+++ Project1/script.js
@@ -5,9 +5,19 @@ function click(){
         }
     }
 }
+function click2(){
+    for(var c = 0; c < checks.length; c++){
+        if(checks[c] == true){
+            console.log(checks[c].value)
+        }
+    }
+}
 
 
 const saida = window.document.getElementById('saida')
+const checks = window.document.querySelectorAll('.check')
 const materias = window.document.getElementsByName('materia')
+const button2 = window.document.getElementById('button2')
 const button1 = window.document.getElementById('button')
 button1.addEventListener('click',click)
+button2.addEventListener('click',click2)
